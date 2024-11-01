934464c4798c315ca8ad23329adc93c9d602a2b0
jon-renzil-08 <johnirevormasiziliwu@gmail.com>
2024-11-01 18:47:41 +0700

    create class components button

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
+import React from "react"
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button className="h-10 px-6 font-semibold bg-black text-white rounded-md">Buy Now</button>
+    )
+  }
+}
 
 
 function App() {
   return (
     <div className="bg-lime-200 min-h-screen flex items-center justify-center">
-      Hellow World
+      <div className="flex gap-4">
+        <Button  />
+        <Button />
+        <Button />
+      </div>
     </div>
    
   )
