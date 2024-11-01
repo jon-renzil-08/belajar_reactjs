ee8c333e496728971fdaf0eef01258243418a77f
jon-renzil-08 <johnirevormasiziliwu@gmail.com>
2024-11-01 19:42:08 +0700

    create function components and properti (props)

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,24 @@
-import React from "react"
 
-class Button extends React.Component {
-  render() {
-    return (
-      <button className="h-10 px-6 font-semibold bg-black text-white rounded-md">Buy Now</button>
-    )
-  }
-}
 
-// create function component start
-const ButttonBlack = () => {
+const Button = (props) => {
+  const { children = "Default Button", variant = "bg-orange-600" } = props;
   return (
-    <button className="h-10 px-6 font-semibold bg-red-500 text-white rounded-md">Buy Now</button>
-  )
-}
-// create function component end
-
+    <button className={`h-10 px-6 ${variant} text-white rounded-md`}>
+      {children}
+    </button>
+  );
+};
 
 function App() {
   return (
-    <div className="bg-lime-200 min-h-screen flex items-center justify-center">
+    <div className="bg-lime-500 min-h-screen flex justify-center items-center">
       <div className="flex gap-4">
-        <Button  />
-        <Button />
-        <Button />
-        <ButttonBlack />
+        <Button variant="bg-orange-600">Login</Button>
+        <Button variant="bg-blue-600">Logout</Button>
+        <Button></Button>
       </div>
     </div>
-   
-  )
+  );
 }
 
-export default App
+export default App;
